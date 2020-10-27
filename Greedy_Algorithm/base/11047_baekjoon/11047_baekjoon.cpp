7baa86e4cb3a853dde857319678b163675cd255d
Kimseonju <tjsdkdlel199@naver.com>
2020-10-27 15:28:46 +0900

    greedy

---
+++ Greedy_Algorithm/base/11047_baekjoon/11047_baekjoon.cpp
@@ -0,0 +1,36 @@
+﻿// 11047_baekjoon.cpp 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int func(int a, int b)
+{
+	return a > b;
+}
+int main()
+{
+	int a, b;
+
+	scanf_s("%d %d", &a, &b);
+	int input;
+	vector<int> coin(11);
+	for (int i = 0; i < a; ++i)
+	{
+		cin >> coin[i];
+	}
+	int sum = 0;
+	int count = 0;
+	sort(coin.begin(), coin.end(), func);
+	for (int i = 0; i < a; i++)
+	{
+
+		while (b - coin[i] >= 0)
+		{
+
+			count++;
+			b -= coin[i];
+		}
+	}
+	cout << count;
+}
---
+++ brute_force/base/7568_baekjoon/7568_baekjoon.cpp
@@ -0,0 +1,19 @@
+﻿// 7568_baekjoon.cpp 
+
+#include <iostream>
+using namespace std;
+
+int x[50], y[50], z[50];
+int main()
+{
+	int a;
+	
+	cin >> a;
+	for (int i = 0; i < a; ++i) {
+		cin >> x[i] >> y[i];
+	}
+
+	int num = 1;
+	for()
+
+}
