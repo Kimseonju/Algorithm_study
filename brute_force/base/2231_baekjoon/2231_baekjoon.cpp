adec71583aff0055400a4216ac7f6540e6e0395c
Kimseonju <tjsdkdlel199@naver.com>
2020-10-27 14:37:53 +0900

    brute

---
+++ brute_force/base/2231_baekjoon/2231_baekjoon.cpp
@@ -0,0 +1,43 @@
+﻿// 2231_baekjoon.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+
+	int N, M;
+	int answer = 0;
+
+	cin >> N;
+
+	int a=N;
+	while (a)
+	{
+		a /= 10;
+		answer++;
+	}
+	//
+	M =N- answer * 9;
+	string k;
+	
+	for (;;) {
+		if (M == N)
+			break;
+		int num = M;
+		k = to_string(M);
+		for (int i = 0; i < k.length(); ++i) {
+			num +=k[i]-'0';
+		}
+		if (num == N)
+			break;
+		else
+			M++;
+	}
+
+	if(M==N)
+		cout << 0;
+	else
+		cout << M;
+}
---
+++ brute_force/base/2798_baekjoon/2798_baekjoon.cpp
@@ -0,0 +1,42 @@
+﻿// 2798_baekjoon.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+int N;
+int M;
+int answer;
+int pluss;
+int base[100];
+bool check[100];
+void func(int x) {
+	if (x == 3) {
+		if(M>=pluss){
+			if (answer < pluss)
+				answer = pluss;
+		}
+		return;
+	}
+	for (int i = 0; i < N; ++i) {
+		if(!check[i]){
+			pluss += base[i];
+			check[i] = true;
+			func(x + 1);
+			pluss -= base[i];
+			check[i] = false;
+		}
+	}
+
+}
+
+int main()
+{
+	cin >> N >> M;
+	for (int i = 0; i < N; ++i) {
+		cin >> base[i];
+	}
+	func(0);
+
+	cout << answer;
+}
+
