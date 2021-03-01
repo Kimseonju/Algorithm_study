6b2f4d7ed6c12a733c8ffd39cfa14281b3179b05
Kimseonju <tjsdkdlel199@naver.com>
2021-03-01 10:15:28 +0900

    위상

---
+++ 16985beakjoon/16985beakjoon.cpp
@@ -0,0 +1,83 @@
+﻿// 16985beakjoon.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <queue>
+using namespace std;
+int board[5][5][5];
+
+bool visited[5][5][5];
+int ans = 999999999; 
+int dx[] = { 0, 1, 0, -1, 0, 0 }, dy[] = { -1, 0, 1, 0, 0, 0 }, dz[] = { 0, 0, 0, 0, 1, -1 };
+
+int n = 5;
+void rotate_board(int idx) {
+
+	int tmp[5][5];
+	memcpy(tmp, board[idx], sizeof(tmp));
+	for (int i = 0; i < 5; i++){
+		for (int j = 0; j < 5; j++)
+		{
+			board[idx][i][j] = tmp[n - 1 - j][i];
+		}
+	}
+}
+struct Point {
+	int x, y, z, i;
+	Point(int ax, int ay, int az, int ai)
+	{
+		x = ax;
+		y = ay;
+		z = az;
+		i = ai;
+	}
+};
+int bfs()
+{
+	memset(visited, false, sizeof(visited));
+
+}
+
+int main()
+{
+	for (int k = 0; k < 5; ++k)
+	{
+		for (int i = 0; i < 5; ++i)
+		{
+			for (int j = 0; j < 5; ++j)
+			{
+				cin >> board[k][i][j];
+			}
+		}
+	}
+
+
+	for (int a = 0; a < 4; ++a)
+	{
+		rotate_board(0); 
+		if (!board[0][0][0])
+			continue;
+		for (int b = 0; b < 4; ++b)
+		{
+			rotate_board(1);
+			for (int c = 0; c < 4; ++c)
+			{
+				rotate_board(2);
+
+				for (int d = 0; d < 4; ++d)
+				{
+
+					rotate_board(3);
+					for (int d = 0; d < 4; ++d)
+					{
+						rotate_board(4); 
+						ans = min(ans, bfs()); 
+					}
+				}
+			}
+		}
+	}
+	cout << ans << '\n';
+
+
+}
---
+++ 위상정렬/3665beakjoon/3665beakjoon.cpp
@@ -0,0 +1,107 @@
+﻿// 3665beakjoon.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <memory.h>
+#include <cstring>
+using namespace std;
+
+int num;
+int n;
+int t[500];
+int m;
+int mat[501][501];
+int in[501];
+int main()
+{
+
+	cin >> num;
+	for (int i = 0; i < num; ++i)
+	{
+		memset(mat, 0, sizeof(mat));
+		memset(in, 0, sizeof(in));
+		memset(t, 0, sizeof(t));
+		
+		cin >> n;
+		for (int i = 1; i <= n; ++i)
+		{
+			cin >> t[i];
+		}
+
+		for (int i = 1; i <= n; ++i)
+		{
+			for (int j = i + 1; j <= n; ++j)
+			{
+				mat[t[i]][t[j]] = 1;
+				in[t[j]]++;
+			}
+		}
+
+		cin >> m;
+		int a, b;
+		for (int i = 0; i < m; ++i)
+		{
+			cin >>a >> b;
+			if (mat[a][b]) {
+				mat[a][b] = 0;
+				mat[b][a] = 1;
+				in[b]--, in[a]++;
+			}
+			else {
+				mat[b][a] = 0;
+				mat[a][b] = 1;
+				in[a]--, in[b]++;
+			}
+		}
+		queue<int> q;
+		vector<int> ans;
+		for (int i = 1; i <= n; ++i)
+		{
+			if (in[i] == 0)
+				q.push(i);
+		}
+		bool check=false;
+		while (!q.empty())
+		{
+			if (q.size() >= 2)
+			{
+				check = true;
+				break;
+			}
+			int z = q.front();
+			q.pop();
+			ans.push_back(z);
+			for (int i = 1; i <= n; ++i)
+			{
+				if (mat[z][i])
+				{
+					in[i]--;
+					if (in[i] == 0)
+					{
+						q.push(i);
+					}
+				}
+			}
+		}
+		if (ans.size() == n)
+		{
+			for (int i = 0; i < n; ++i)
+			{
+				cout << ans[i] << " ";
+			}
+			cout << "\n";
+		}
+		else if(check)
+		{
+			cout << "?" << endl;
+		}
+		else
+			cout << "IMPOSSIBLE" << endl;
+
+
+	}
+	
+
+}
