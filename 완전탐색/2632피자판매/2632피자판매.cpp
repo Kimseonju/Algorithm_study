721c0a69ba4cd2b72658d19d881a1089ce74a72b
Kimseonju <tjsdkdlel199@naver.com>
2021-07-02 13:24:42 +0900

    no message

---
+++ 완전탐색/2632피자판매/2632피자판매.cpp
@@ -0,0 +1,104 @@
+﻿
+#include <iostream>
+#include <vector>
+#include<algorithm>
+using namespace std;
+
+
+int sizex;
+int m, n;
+
+vector<int> M;
+vector<int> N;
+int main()
+{
+    cin >> sizex;
+
+    cin >> m >> n;
+    int num;
+    for (int i = 0; i < m; ++i)
+    {
+        cin >> num;
+        M.push_back(num);
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> num;
+        N.push_back(num);
+    }
+    vector<int> arr1;
+    vector<int> arr2;
+    vector<int> arr3;
+
+    for (int i = 0; i < m; ++i)
+    {
+        int temp=0;
+        temp += M[i];
+        arr1.push_back(temp);
+        int checki = i;
+        checki--;
+        if (checki < 0)
+            checki = m-1;
+        for (int j = i + 1; j != checki; ++j)
+        {
+            if (j == m)
+            {
+                j = -1;
+                continue;
+            }
+            temp += M[j];
+            arr1.push_back(temp);
+        }
+    }
+    int sum=0;
+    for (int i = 0; i < m; ++i)
+    {
+        sum += M[i];
+    }
+    arr1.push_back(sum);
+
+
+    for (int i = 0; i < n; ++i)
+    {
+        int temp = 0;
+        temp += N[i];
+        arr2.push_back(temp);
+        int checki = i;
+        checki--;
+        if (checki < 0)
+            checki = n - 1;
+        for (int j = i + 1; j != checki; ++j)
+        {
+            if (j == n)
+            {
+                j = -1;
+                continue;
+            }
+            temp += N[j];
+            arr2.push_back(temp);
+        }
+    }
+    sum = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        sum += N[i];
+    }
+    arr2.push_back(sum);
+
+    int ans = 0;
+    sort(arr1.begin(), arr1.end());
+    sort(arr2.begin(), arr2.end());
+
+    for (auto item : arr1)
+    {
+        int diff = sizex - item;
+        int ub = upper_bound(arr2.begin(), arr2.end(), diff) - arr2.begin();
+        int lb = lower_bound(arr2.begin(), arr2.end(), diff) - arr2.begin();
+        ans += (ub - lb);
+        if (diff == 0)
+            ans++;
+    }
+
+
+    cout << ans;
+}
---
+++ 완전탐색/7453합이0인네정수/7453합이0인네정수.cpp
@@ -0,0 +1,57 @@
+﻿
+
+#include <iostream>
+#include <vector>
+#include<algorithm>
+using namespace std;
+
+
+int N;
+int main()
+{
+	cin >> N;
+
+	vector<long long> A(N);
+	vector<long long> B(N);
+	vector<long long> C(N);
+	vector<long long> D(N);
+
+	vector<long long> Arr1;
+	vector<long long> Arr2;
+
+	
+	for (int i = 0; i < N; ++i)
+	{
+		int a, b, c, d;
+		cin >> a >> b >> c >> d;
+
+		A[i] = a;
+		B[i] = b;
+		C[i] = c;
+		D[i] = d;
+	}
+
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < N; ++j)
+		{
+			Arr1.push_back(A[i] + B[j]);
+			Arr2.push_back(C[i] + D[j]);
+		}
+	}
+	long long answer = 0;
+	sort(Arr1.begin(), Arr1.end());
+	sort(Arr2.begin(), Arr2.end());
+	int pre;
+		int end;
+	for (int i = 0; i < Arr1.size(); i++) {
+
+		pre = lower_bound(Arr2.begin(), Arr2.end(), -Arr1[i]) - Arr2.begin();
+		end = upper_bound(Arr2.begin(), Arr2.end(), -Arr1[i]) - Arr2.begin();
+
+		answer += (end - pre);
+	}
+
+	cout << answer;
+
+}
