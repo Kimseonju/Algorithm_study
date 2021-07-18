6dce40c6a1c8d548264eb283925dc73ee36c1579
Kimseonju <tjsdkdlel199@naver.com>
2021-07-18 13:46:37 +0900

    no message

---
+++ 문자열알고리즘/1305광고/1305광고.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> preprocessing(int l, string p) {
+    vector<int> pi(l, 0);
+    int j = 0;
+    for (int i = 1; i < l; i++)
+    {
+        while (j > 0 && p[i] != p[j])
+            j = pi[j - 1];
+        if (p[i] == p[j])
+        {
+            pi[i] = j + 1;
+            j++;
+        }
+    }
+    return pi;
+}
+int main()
+{
+    int l;
+    cin >> l;
+    string p;
+    cin >> p;
+
+    vector<int> ans = preprocessing(l, p);
+
+    cout << l - ans[l - 1];
+}
