9287a7dbbd09be0e893d7b45e61b02bec27586b6
Kimseonju <tjsdkdlel199@naver.com>
2021-08-09 01:41:40 +0900

    no message

---
+++ 분할정복을 이용한 곱셈/1629곱셈/1629곱셈.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+using namespace std;
+long long int N, M, K;
+long long int fpowa(long long int C, long long int n)
+{
+	if (n == 1) 
+		return C % K;
+
+	long long int ans = fpowa(C, n / 2);
+
+	ans = (ans * ans) % K;
+	if (n % 2)
+		ans = ans * C % K;
+
+	return ans;
+
+}
+int main()
+{
+	cin >> N >> M >> K;
+
+
+	long long int c = fpowa(N, M);
+
+	cout << c;
+}
