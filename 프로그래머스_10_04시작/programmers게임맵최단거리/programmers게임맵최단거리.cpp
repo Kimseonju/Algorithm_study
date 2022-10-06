c6b862a9ec5c67728b9c93c67b5288c3268b3a6b
Kimseonju <tjsdkdlel199@naver.com>
2022-10-06 13:01:14 +0900

    10_06

--- 프로그래머스_10_04시작/programmers게임맵최단거리/programmers게임맵최단거리.cpp
+++ 프로그래머스_10_04시작/programmers게임맵최단거리/programmers게임맵최단거리.cpp
@@ -1,20 +1,51 @@
-﻿// programmers게임맵최단거리.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <iostream>
-
-int main()
+﻿#include<vector>
+#include <queue>
+#include <tuple>
+using namespace std;
+int MoveCount[101][101];
+int dx[4] = { 0,1,0,-1 };
+int dy[4] = { 1,0,-1,0 };
+int solution(vector<vector<int> > maps)
 {
-    std::cout << "Hello World!\n";
+    int answer = 0;
+    queue<tuple<int, int, int>> q;
+    int n = maps.size();
+    int m = maps[0].size();
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            MoveCount[i][j] = 50000;
+        }
+    }
+    MoveCount[0][0] = 1;
+    q.push(make_tuple(0, 0, 1));
+    while (!q.empty())
+    {
+        int Y = get<0>(q.front());
+        int X = get<1>(q.front());
+        int NowMoveCount = get<2>(q.front());
+        q.pop();
+        if (NowMoveCount > MoveCount[Y][X])
+            continue;
+        for (int i = 0; i < 4; ++i)
+        {
+            int NextX = dx[i] + X;
+            int NextY = dy[i] + Y;
+            if (NextY < 0 || NextY >= n || NextX < 0 || NextX >= m)
+                continue;
+            if (maps[NextY][NextX] == 0)
+                continue;
+            if (MoveCount[NextY][NextX] > NowMoveCount + 1)
+            {
+                MoveCount[NextY][NextX] = NowMoveCount + 1;
+                q.push(make_tuple(NextY, NextX, NowMoveCount + 1));
+            }
+        }
+    }
+    answer = MoveCount[n - 1][m - 1];
+    if (answer == 50000)
+        return -1;
+    else
+        return answer;
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
