aca218f19eeca5ad378a6730df76774f36482cce
Kimseonju <tjsdkdlel199@naver.com>
2022-10-12 12:58:57 +0900

    10_12

---
+++ 프로그래머스_10_04시작/가장큰숫자/가장큰숫자.cpp
@@ -0,0 +1,39 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string GetBinaryNumber(int n)
+{
+    string ret = "";
+    while (n > 0)
+    {
+        int remain = n % 2;
+        n /= 2;
+        ret = to_string(remain) + ret;
+    }
+    return ret;
+}
+int GetBinaryCount(string Binarystr)
+{
+    int ret = 0;
+    for (int i = 0; i < Binarystr.size(); ++i)
+    {
+        if (Binarystr[i] == '1')
+            ret++;
+    }
+    return ret;
+}
+int solution(int n)
+{
+    int answer = 0;
+    string binarystr_n = GetBinaryNumber(n);
+    int binaryCount = GetBinaryCount(binarystr_n);
+    for (int i = n + 1;; i++)
+    {
+        string binarystr_i = GetBinaryNumber(i);
+        int binaryCount_i = GetBinaryCount(binarystr_i);
+        if (binaryCount == binaryCount_i)
+            return i;
+    }
+}
---
+++ 프로그래머스_10_04시작/올바른괄호/올바른괄호.cpp
@@ -0,0 +1,26 @@
+﻿#include<string>
+#include <iostream>
+#include <deque>
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    deque<char> st;
+    for (int i = 0; i < s.size(); ++i)
+    {
+        if (s[i] == ')')
+        {
+            if (st.size() == 0)
+                return false;
+            if (st[0] != '(')
+                return false;
+            st.pop_front();
+            continue;
+        }
+        st.push_back(s[i]);
+    }
+    if (st.size() != 0)
+        return false;
+    return true;
+}
