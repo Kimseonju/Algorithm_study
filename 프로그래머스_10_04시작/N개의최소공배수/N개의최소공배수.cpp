51e538a30156b77b76fbfdb81cdcf5461a96ccb5
Kimseonju <tjsdkdlel199@naver.com>
2022-10-13 15:25:49 +0900

    10_13

---
+++ 프로그래머스_10_04시작/N개의최소공배수/N개의최소공배수.cpp
@@ -0,0 +1,25 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int gcd(int num1, int num2)
+{
+    if (num2 == 0) return num1;
+    else return gcd(num2, num1 % num2);
+}
+int solution(vector<int> arr)
+{
+    int answer = 0;
+    sort(arr.begin(), arr.end());
+    if (arr.size() == 1)
+        return arr[0];
+    for (int i = 0; i < arr.size(); ++i)
+    {
+        arr[i + 1] = (arr[i] * arr[i + 1]) / gcd(arr[i + 1], arr[i]);
+    }
+
+
+
+    return arr[arr.size() - 1];
+}
---
+++ 프로그래머스_10_04시작/예상대진표/예상대진표.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+    int answer = 0;
+
+
+    if (abs(a - b) == 1)
+    {
+        int nexta = a / 2 + a % 2;
+        int nextb = b / 2 + b % 2;
+        if (abs(nexta - nextb) == 0)
+            return 1;
+    }
+
+    for (int i = 2;; ++i)
+    {
+        int nexta = a / 2 + a % 2;
+        int nextb = b / 2 + b % 2;
+        if (abs(nexta - nextb) == 0)
+            return i - 1;
+        a = nexta;
+        b = nextb;
+    }
+    return -1;
+}
